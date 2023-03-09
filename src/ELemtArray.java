c6d7c338549d0ec33edf660c05d90d368e7b7fe4
gurunathreddy2727@gmail.com <gurunathreddy2727@gmail.com>
2023-03-10 00:39:50 +0530

    Element array

--- src/ELemtArray.java
+++ src/ELemtArray.java
@@ -1,2 +1,9 @@
-public class ELemtArray {
+class ElemtArray {
+    public static void main(String[] args) {
+        String[] array = {"ram", "sita", "radha", "Krishna"};
+        System.out.println("Array elements are:");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
 }
