a9b45f4c434ea86d627df2026ad22f1e6320b1a8
gurunathreddy2727@gmail.com <gurunathreddy2727@gmail.com>
2023-03-10 12:22:14 +0530

    reverse order

--- src/ReverseOrder.java
+++ src/ReverseOrder.java
@@ -1,9 +1,7 @@
 class ReverseOrder {
     public static void main(String[] args) {
-        // Declare and initialize an array of integers
         int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-        // Loop through the array in reverse order and print elements
         for (int i = array.length - 1; i >= 0; i--) {
             System.out.println(array[i]);
         }
