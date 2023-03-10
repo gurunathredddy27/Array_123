50e21da16d8dfa0badc394974d10d8acb1c72430
gurunathreddy2727@gmail.com <gurunathreddy2727@gmail.com>
2023-03-10 12:06:21 +0530

    Even Position

---
+++ src/EvenPosition.java
@@ -0,0 +1,8 @@
+public class EvenPosition {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        for (int i = 0; i < array.length; i += 2) {
+            System.out.println(array[i]);
+        }
+    }
+}
